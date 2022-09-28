f501022c134b88c84db01760c868adede88977b0
HueNguyen <101964205+HueNguyenSE@users.noreply.github.com>
2022-09-28 15:27:14 +1000

    update
    Handled the display in multiple scenarios of search.
    Two minor issues not fixed yet.

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -3,12 +3,17 @@ import React from 'react';
 import axios from 'axios';
 
 function App() {
-	console.log('App');
+	//console.log('App');
 	const [countries, setCountries] = useState([]);
+	const [filter, setFilter] = useState([]);
+	const [matches, setMatches] = useState([]);
+
+	const len = countries.length; //number of countries in the database
+	const allContainers = document.querySelectorAll('.country-container');
+	const countryNames = document.querySelectorAll('.country-name');
 
 	//fetch data
 	const effectHandler = () => {
-		console.log('effect');
 		axios.get('https://restcountries.com/v3.1/all').then((response) => {
 			setCountries(countries.concat(response.data));
 		});
@@ -16,16 +21,32 @@ function App() {
 	useEffect(effectHandler, []);
 	console.log('countries', countries);
 
-	const len = countries.length;
-	const names = countries.map((country) => country.name.common);
-	const areas = countries.map((country) => country.area);
+	//get name of all countries
+	const names = countries.map((country) => {
+		if (country.name.common !== undefined) {
+			return country.name.common;
+		} else {
+			return 'no data to show';
+		}
+	});
+	//get area of all countries
+	const areas = countries.map((country) => {
+		if (country.area !== undefined) {
+			return country.area;
+		} else {
+			return 'no data to show';
+		}
+	});
+	//capiital(s) of countries
 	const capitals = countries.map((country) => {
+		//get capital of all countries
 		if (country.capital !== undefined) {
 			return country.capital;
 		} else {
 			return ['no data to show'];
 		}
 	});
+	//official languages
 	const languages = countries.map((country) => {
 		if (country.languages !== undefined) {
 			return Object.values(country.languages);
@@ -33,6 +54,7 @@ function App() {
 			return ['no data to show'];
 		}
 	});
+	//flag iamge
 	const flags = countries.map((country) => {
 		if (country.flags !== undefined) {
 			return Object.values(country.flags);
@@ -52,95 +74,97 @@ function App() {
 		});
 	}
 
-	const displayAll = all.map((country, index) => {
-		return (
-			<div id={index} className='country-container'>
-				<h1 className='name'>{country.name}</h1>
-				<p>Capital: {country.capital.map((cap) => `${cap} `)}</p>
-				<p>Area: {country.area} </p>
-				<p>
-					<b>Languages:</b>{' '}
-				</p>
-				<ul>
-					{country.languages.map((lang) => (
-						<li>{lang}</li>
-					))}
-				</ul>
-				<img
-					src={country.flags[0]}
-					alt='country flag'
-					width='400'
-					height='300'
-				/>
-			</div>
-		);
-	});
+	//handle hide/show country details
+	function showCountryDetails(index) {
+		const targetContainer = allContainers[index];
+		if (targetContainer.style.display === 'none') {
+			targetContainer.style.display = 'block';
+		} else {
+			targetContainer.style.display = 'none';
+		}
+	}
 
-	//show details of each country
-	let displayOne;
-	//function handleSearch
-	function handleSearch() {
+	//handle searching
+	const handleSearch = () => {
 		//get the input value
 		const input = document.getElementById('search-input');
-		const filter = input.value.toLowerCase();
-		console.log('filter', filter);
+		const inputValue = input.value;
+		setFilter(inputValue.toUpperCase());
+		console.log('search keys:', filter);
 
 		//an array storing all indexes of matched countries
-		let matchCases = [];
-		//loop through countries and hide those don't match
-		for (let i = 0; i < len; i++) {
-			if (names[i].toLowerCase().indexOf(filter) > -1) {
-				matchCases.push(i);
-			}
-		}
+		setMatches(
+			names.reduce((allMatches, currentName, currentIndex) => {
+				if (currentName.toUpperCase().indexOf(filter) > -1) {
+					allMatches.push(currentIndex);
+				}
+				return allMatches;
+			}, [])
+		);
+		console.log('match indexes: ', matches);
 		const main = document.getElementById('main');
+
 		const p = document.createElement('p');
-		const textNode = document.createTextNode('Too many cases found to display');
+		const textNode = document.createTextNode('');
 		p.appendChild(textNode);
 		main.appendChild(p);
 		p.style.display = 'none';
 
-
-		if (matchCases.length <= 10) {
-			for (let i of matchCases) {
-				const textNodeForName = document.createTextNode(names[i]);
-				const pForName = document.createElement('p');
-				pForName.appendChild(textNodeForName);
-
-				const button = document.createElement('button');
-				const buttonText = document.createTextNode('show');
-				button.appendChild(buttonText);
-
-				pForName.appendChild(button);
-				main.appendChild(pForName);
-
-				button.onclick = () => {
-					console.log('button clicked');
-				};
+		if (matches.length <= 10 && matches.length >= 2) {
+			for (let i of matches) {
+				countryNames[i].style.display = 'block';
 			}
-		} else if (matchCases.length === 1) {
-			p.style.display = 'none';
-			const i = matchCases[0];
-			displayOne = displayAll[i];
-			console.log('view', displayOne);
-
+		} else if (matches.length === 1) {
+			const i = matches[0];
+			countryNames[i].style.display = 'none';
+			allContainers[i].style.display = 'block';
+		} else if (matches.length === 0) {
+			textNode.textContent = 'There is no match found';
+			p.style.display = 'block';
 		} else {
-			//handle the case when there are more than 10 matches
-			p.style.display = '';
+			//too many cases
+			textNode.textContent = 'Too many matches found';
+			p.style.display = 'block';
 		}
-
-		console.log('matchCases: ', matchCases, matchCases.length);
-		//display match cases
-		console.log('display one', displayOne)
 	}
 
 	return (
 		<div id='main'>
-			<p>
-				Find countries{' '}
+			<div>
+				<label for="search-input">Find countries{' '}</label>
 				<input type='text' id='search-input' onKeyUp={handleSearch} />
-			</p>
-			<div id="all"></div>
+			</div>
+			{names.map((name, index) => (
+				<div className='country'>
+					<p className='country-name' style={{ display: 'none' }}>
+						{name}
+						<button onClick={() => showCountryDetails(index)}>show</button>
+					</p>
+					<div
+						id={index}
+						className='country-container'
+						style={{ display: 'none' }}
+					>
+						<h1 className='name'>{names[index]}</h1>
+						<p>Capital: {capitals[index].map((cap) => `${cap} `)}</p>
+						<p>Area: {areas[index]} </p>
+						<p>
+							<b>Languages:</b>{' '}
+						</p>
+						<ul>
+							{languages[index].map((lang) => (
+								<li>{lang}</li>
+							))}
+						</ul>
+						<img
+							src={flags[index][0] || flags[index][1]}
+							alt='country flag'
+							width='400'
+							height='300'
+						/>
+					</div>
+				</div>
+			))}
 		</div>
 	);
 }
